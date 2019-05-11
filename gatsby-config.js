9b4329a1cb746b648834d91f708d112b4d1d6b61
gastoire <buzzcutseason73@gmail.com>
2019-05-11 01:36:52 -0300

    Datos de gatsby-config modificados

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,8 +57,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Mónica Marín WebSite`,
+        short_name: `MónicaMarínAPP`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
